811a0ecd7442b4559c09426c09e899f811d05103
unknown <bhandariruchi498@gmail.com>
2023-08-15 21:00:39 +0530

    messgae

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ const App = () => {
          <h2>Child Component 2</h2> 
          <button onClick={()=> setSelect("Option 2")}>Option 2</button>
       </div>
-      <p>Selected Option : {select}</p>
+      <p>Selected Option: {select}</p>
     </div>
   )
 }
