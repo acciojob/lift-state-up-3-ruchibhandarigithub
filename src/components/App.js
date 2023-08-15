23ea5c225e5a03136a263599ef4fea53ff2497cb
unknown <bhandariruchi498@gmail.com>
2023-08-15 20:51:44 +0530

    message

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [select,setSelect]=useState();
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent">
+      <h1>Parent Component</h1>
+      <div className="child-1">
+        <h2>Child Component 1</h2> 
+        <button onClick={()=> setSelect("Option 1")}>Option 1</button>
+      </div>
+      <div className="child-2">
+         <h2>Child Component 2</h2> 
+         <button onClick={()=> setSelect("Option 2")}>Option 2</button>
+      </div>
+      <p>Selected Option : {select}</p>
     </div>
   )
 }
